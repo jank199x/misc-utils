df36375f8e7d26d0af1c6f2fed47f180458267d7
Jank <jank@panichev.xyz>
2022-05-16 08:46:57 +0400

    Rename for clarity

--- flatten-single-file-dirs.py
+++ flatten-single-file-dirs.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
 
 import os
-rootdir = '/home/pavle/Important/smolness/digital-trash'
+
+rootdir = "/home/pavle/Important/smolness/digital-trash"
 
 for subdir, dirs, files in os.walk(rootdir):
     if len(files) == 1:
-        print(f'\n{subdir}: {len(files)}')
+        print(f"\n{subdir}: {len(files)}")
         file = files[0]
         filepath = os.path.join(subdir, file)
         print(f"{filepath=}")
         subdirname = os.path.basename(subdir)
         _, ext = os.path.splitext(file)
         print(f"{subdirname=} {ext=}")
-        parentdir= os.path.dirname(subdir)
+        parentdir = os.path.dirname(subdir)
         print(f"{parentdir=}")
-        renameto = os.path.join(parentdir, subdirname+ext)
+        renameto = os.path.join(parentdir, subdirname + ext)
         print(f"{renameto=}")
         os.rename(filepath, renameto)
 
