830832ba2b89f087ba96969114113e4887a620c8
Jank <jank@panichev.xyz>
2022-07-21 17:27:31 +0300

    Fix bugs in zsh history formatter

--- zsh-history-to-md.py
+++ zsh-history-to-md.py
@@ -1,26 +1,32 @@
 #!/usr/bin/env python3
 
+from datetime import datetime
 import sys
 from typing import DefaultDict
 
 
 def main():
     history = DefaultDict(list)
-    with open(sys.argv[1], "r") as f:
+    with open(sys.argv[1], "rb") as f:
         for line in f.readlines():
+            line = line.decode('utf-8',errors='ignore')
+            line = line.split(";")[-1]
             try:
                 if line.split()[0] == "sudo":
                     cmd = line.split()[1]
                 else:
                     cmd = line.split()[0]
-                history[cmd].append(line.strip())
+                if line.strip() not in history[cmd]:
+                    history[cmd].append(line.strip())
             except:
                 # print(f"Couldn't split line {line}")
                 pass
+    today = datetime.now().date().isoformat()
+    print(f"# ZSH History {today}\n", file=sys.stdout)
     for k, v in history.items():
-        print(f"# {k}")
+        print(f"## {k}\n", file=sys.stdout)
         for item in sorted(v):
-            print(f"`{item}`\n")
+            print(f"`{item}`\n", file=sys.stdout)
 
 
 if __name__ == "__main__":
